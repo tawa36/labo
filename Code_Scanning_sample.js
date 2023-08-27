464c136e4390a3ef8e384880c20f578e13b1a00d
tawa36 <hsasaki.198804@gmail.com>
2023-08-27 12:38:21 +0900

    Update Code_Scanning_sample.js

--- Code_Scanning_sample.js
+++ Code_Scanning_sample.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log('Server listening on port 3000desktop');
+  console.log('Server listening on port 3000desktop_branchdesk');
 });
