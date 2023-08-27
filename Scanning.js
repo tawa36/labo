e37e5bf1883c5fdbf14466252a70f6b70acd0b4d
tawa36 <139942686+tawa36@users.noreply.github.com>
2023-08-27 15:58:29 +0900

    Update Scanning.js

--- Scanning.js
+++ Scanning.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get('/', (req, res) => {
   const userComment = req.query.comment || '';
-  //res.send(`<html><body><h1>User comment: ${userComment}</h1></body></html>`);
+  ////res.send(`<html><body><h1>User comment: ${userComment}</h1></body></html>`);
 });
 
 app.listen(3000, () => {
