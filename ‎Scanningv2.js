1404a11773fabadc04ca6b280192f2b601807704
tawa36 <139942686+tawa36@users.noreply.github.com>
2025-06-05 19:31:44 +0900

    Delete ‎Scanningv2.js

--- ‎Scanningv2.js
+++
@@ -1,5 +0,0 @@
-app.get('/', (req, res) => {
-  const userComment = req.query.comment || '';
-  res.send(`<html><body><h1>User comment: ${userComment}</h1></body></html>`);
-  //res.send(`<html><body><h1>User comment: ${userComment}</h1></body></html>`);
-});
